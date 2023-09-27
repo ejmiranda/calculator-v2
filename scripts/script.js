fe812c53b2ff194c8b8b0b2957d6aa406c83142a
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-26 22:31:38 -0500

    Add Calculator constructor with operations

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,27 @@ buttons.forEach(button => {
     displayText.textContent = button.getAttribute('id');
   });
 });
+
+function Calculator() {
+  this.operations = {
+    '+': (n1, n2) => n1 + n2,
+    '-': (n1, n2) => n1 - n2,
+    'x': (n1, n2) => n1 * n2,
+    '÷': (n1, n2) => n1 / n2,
+    '%': (n1) => n1 / 100,
+    '+/-': (n1) => n1 * -1,
+  }
+  this.calculate = (n1, sign, n2) => {
+    let result = '';
+    result = this.operations[sign](+n1, +n2)
+    return result;
+  }
+}
+
+let calc = new Calculator();
+console.log(calc.calculate(2, '+', 2));
+console.log(calc.calculate(5, '-', 2));
+console.log(calc.calculate(5, 'x', 10));
+console.log(calc.calculate(50, '÷', 2));
+console.log(calc.calculate(9, '%'));
+console.log(calc.calculate(100, '+/-'));
